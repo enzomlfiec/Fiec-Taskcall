7614dbd557d8334148c3ef27dbeeca68eba728d4
Natan <natan46091@fiec.edu.br>
2026-09-13 19:14:12 -0300

    Migrate Painel -> TelaPrincipal e Sidebar -> Telaprincipal e  Inbox

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,25 +1,35 @@
-import SideBar from "./components/SideBar";
 import Painel from "./components/Painel";
 import "../style.css";
 import FakeLayers from "./components/FakeLayers";
-import { Routes, Route } from "react-router-dom";
 import Inbox from "./components/Inbox";
-import TelaPrincipal from "./components/TelaPrimcipal";
+import TelaPrincipal from "./components/TelaPrincipal";
+
+{/* Rotas */}
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>{/* 
+    <>
       {
       <Routes>
 
         <Route path="/" element={<FakeLayers/>}/> 
         <Route path="/painel" element={<Painel/>}/>
 
+        <Route path="/tela-principal" element={<TelaPrincipal />} />
+
+        <Route path="/inbox" element={<Inbox />} />
+
+        {/* Futuras rotas de chamado, usuário e configurações
+
+         <Route  path="/chamados" element={<h1 className="text-white">Chamados</h1>}/>
+
+         <Route  path="/configuracoes" element={<h1 className="text-white">Configurações</h1>}/>
+
+          <Route path="/usuario "element={<h1 className="text-white">Usuário</h1>} />
+       */}
       </Routes>
       }
-      */}
-
-      <TelaPrincipal/>
 
     </>
   );
--- src/frontend/src/components/Painel.jsx
+++ src/frontend/src/components/Painel.jsx
@@ -2,9 +2,10 @@ import { useNavigate } from "react-router-dom"
 
 function Painel() {
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     return (
+
         <div className="flex flex-col h-screen w-screen items-center justify-center bg-[url(public/assets/login2.png)]">
             <img id="logo" src="public/assets/logo/logo-hor.png" className="w-125"></img>
             <div className=" absolute flex flex-col text-white bg-fundo-medio p-20 pr-30 pl-30 gap-3 rounded-4xl border-sec border-2">
@@ -22,8 +23,9 @@ function Painel() {
                 </div>
                 <input className="text-black bg-white w-lg rounded-md p-2" type="password" placeholder="*Insira a senha vinculada ao perfil." />
                 <br></br>
+
                 <div className="flex items-center justify-center">
-                    <button className="bg-accent botao desabilitado"> Entrar </button>
+                    <button onClick={() => navigate("/tela-principal")} className="bg-accent botao "> Entrar </button>
                 </div>
             </div>
         </div>
